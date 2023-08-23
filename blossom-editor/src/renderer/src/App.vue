9da412e6b7b36f4ad24b6e5f6c169881f7d1bf37
jasminexz <491548320@qq.com>
2023-08-23 19:49:59 +0800

    增加边框

--- blossom-editor/src/renderer/src/App.vue
+++ blossom-editor/src/renderer/src/App.vue
@@ -23,6 +23,7 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
   @include box(100%, 100%);
   overflow: hidden;
   box-shadow: 3px 3px 10px 3px rgba(107, 104, 104, 0.3);
+  border: 1px solid rgb(31, 31, 31);
 
   .app-header {
     @include box(100%, 30px);
