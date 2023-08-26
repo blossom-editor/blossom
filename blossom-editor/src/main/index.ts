4310aa4c195393f341394916f98a85e1c312b064
jasminexz <491548320@qq.com>
2023-08-26 15:14:40 +0800

    应用单例启动

--- blossom-editor/src/main/index.ts
+++ blossom-editor/src/main/index.ts
@@ -11,43 +11,63 @@ let mainWindow: BrowserWindow | undefined
 let tray: Tray
 let blossomUserinfo: any
 
-// This method will be called when Electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs.
-/**
- * =========================================================================================================================
- * APP 启动完成
- * =========================================================================================================================
- */
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-  /*
-   * Default open or close DevTools by F12 in development and ignore CommandOrControl + R in production.
-   * see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-   */
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+const additionalData = { blossomSingle: 'blossomSingle' }
+const gotTheLock = app.requestSingleInstanceLock(additionalData)
 
-  setTimeout(() => {
-    createMainWindow()
-  }, 300);
+if (!gotTheLock) {
+  app.quit()
+} else {
 
+  app.on('second-instance', (_event, _commandLine, _workingDirectory, additionalData) => {
+    // 输出从第二个实例中接收到的数据
+    console.log(additionalData)
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
-  })
-
-  // Quit when all windows are closed, except on macOS. There, it's common
-  // for applications and their menu bar to stay active until the user quits
-  // explicitly with Cmd + Q.
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
+    // 有人试图运行第二个实例，我们应该关注我们的窗口
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
     }
   })
-})
+
+  // This method will be called when Electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs.
+  /**
+   * =========================================================================================================================
+   * APP 启动完成
+   * =========================================================================================================================
+   */
+  app.whenReady().then(() => {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
+    /*
+     * Default open or close DevTools by F12 in development and ignore CommandOrControl + R in production.
+     * see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+     */
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    setTimeout(() => {
+      createMainWindow()
+    }, 300);
+
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+    })
+
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+  })
+}
+
 
 
 /** 
@@ -235,7 +255,7 @@ const initOnFocusedWindow = (): void => {
    * 隐藏窗口
    */
   ipcMain.on('window-hide', () => {
-    
+
     if (BrowserWindow.getFocusedWindow()?.id != mainWindow!.id) {
       BrowserWindow.getFocusedWindow()?.close()
     } else {
