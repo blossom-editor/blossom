ad549251aac9fda951eb810a820abe6864219483
xiaozzzi <42293085+xiaozzzi@users.noreply.github.com>
2023-10-02 19:51:42 +0800

    修改脚本

--- blossom-backend/script/restart-springboot.sh
+++ blossom-backend/script/restart-springboot.sh
@@ -5,5 +5,5 @@ echo "进程ID : " $pid
 kill -9 $pid
 echo "进程" $pid "已被杀死"
 echo "开始重启 backend-blossom 服务器"
-nohup java -Xms1024m -Xmx1024m -jar /usr/local/jasmine/blossom/backend/backend-blossom.jar &
+nohup java -Xms1024m -Xmx1024m -jar /usr/local/jasmine/blossom/backend/backend-blossom.jar --spring.profiles.active=prod &
 echo "backend-blossom 正在启动,请查看日志 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓"
