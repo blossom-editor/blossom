dfdbdbf9898f5d989a618b44036081f9fc598660
xiaozzzi <42293085+xiaozzzi@users.noreply.github.com>
2023-10-06 14:57:48 +0800

    支持重复上传和强制删除

---
+++ blossom-backend/backend/src/main/java/com/blossom/backend/server/picture/pojo/PictureDelReq.java
@@ -0,0 +1,27 @@
+package com.blossom.backend.server.picture.pojo;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * 删除图片
+ *
+ * @since 1.6.0 增加忽略图片引用参数
+ */
+@Data
+public class PictureDelReq {
+
+    /**
+     * ID
+     */
+    @NotNull(message = "ID为必填项")
+    @Min(value = 0, message = "ID不能小于0")
+    private Long id;
+
+    /**
+     * 忽略图片引用检查
+     */
+    private Boolean ignoreCheck;
+}
