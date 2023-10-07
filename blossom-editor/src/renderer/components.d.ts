7c4591cf1db6bbeb39b2b7a742688454a99133e2
xiaozzzi <42293085+xiaozzzi@users.noreply.github.com>
2023-10-07 10:35:28 +0800

    增加组件引入

--- blossom-editor/src/renderer/components.d.ts
+++ blossom-editor/src/renderer/components.d.ts
@@ -32,7 +32,6 @@ declare module 'vue' {
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
-    ElImageVi: typeof import('element-plus/es')['ElImageVi']
     ElImageViewer: typeof import('element-plus/es')['ElImageViewer']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
