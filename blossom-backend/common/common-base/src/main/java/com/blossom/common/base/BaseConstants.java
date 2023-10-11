f661315424ef9543e009d7588898a11107d11faa
xiaozzzi <42293085+xiaozzzi@users.noreply.github.com>
2023-10-11 21:49:23 +0800

    新增启动成功配置项日志，用于启动后的配置检查

--- blossom-backend/common/common-base/src/main/java/com/blossom/common/base/BaseConstants.java
+++ blossom-backend/common/common-base/src/main/java/com/blossom/common/base/BaseConstants.java
@@ -27,13 +27,24 @@ public final class BaseConstants {
                         "\n项目名称: [{}]" +
                         "\n当前环境: [{}], 版本: [{}]" +
                         "\n访问地址: http://127.0.0.1:{}{}" +
-                        "\n" +
+                        "\n=============================================================" +
+                        "\n数据库配置: {}" +
+                        "\n数据库用户: {}" +
+                        "\n数据库密码: {}" +
+                        "\n图片前缀: {}" +
+                        "\n图片存储: {}" +
+                        "\n=============================================================" +
                         "\n启动成功: 可下载客户端登录, 默认用户名密码: blos/blos" +
                         "\n下载地址: https://github.com/blossom-editor/blossom/releases" +
                         "\n=============================================================",
                 SpringUtil.getAppName(),
                 SpringUtil.getProfileAction(), SpringUtil.get("project.base.version"),
-                SpringUtil.getPort(), getContextPath()
+                SpringUtil.getPort(), getContextPath(),
+                SpringUtil.get("spring.datasource.url"),
+                SpringUtil.get("spring.datasource.username"),
+                SpringUtil.get("spring.datasource.password"),
+                SpringUtil.get("project.iaas.blos.domain"),
+                SpringUtil.get("project.iaas.blos.default-path")
         );
     }
 
