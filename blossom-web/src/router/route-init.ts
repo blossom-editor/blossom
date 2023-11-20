e3855dd01f20ef4741c56ae7a1f37437d8bfaba3
xiaozzzi <42293085+xiaozzzi@users.noreply.github.com>
2023-11-20 16:43:49 +0800

    补充路由

--- blossom-web/src/router/route-init.ts
+++ blossom-web/src/router/route-init.ts
@@ -13,6 +13,7 @@ const Login = () => import('@/views/index/Login.vue')
 const Articles = () => import('@/views/article/Articles.vue')
 const TodoIndex = () => import('@/views/todo/TodoIndex.vue')
 const PlanIndex = () => import('@/views/plan/PlanIndex.vue')
+const NoteIndex = () => import('@/views/note/NoteIndex.vue')
 
 router.addRoute({ path: '/404', component: NotFound })
 router.addRoute({ path: '/:pathMatch(.*)', redirect: '/404' })
@@ -25,8 +26,9 @@ router.addRoute({
   children: [
     { path: '/home', name: 'Home', component: Home, meta: { keepAlive: true } },
     { path: '/login', name: 'Login', component: Login, meta: { keepAlive: true } },
-    { path: '/articles', name: 'Articles', component: Articles, meta: { keepAlive: false } },
-    { path: '/todo', name: 'TodoIndex', component: TodoIndex, meta: { keepAlive: false } },
-    { path: '/plan', name: 'PlanIndex', component: PlanIndex, meta: { keepAlive: false } }
+    { path: '/articles', name: 'Articles', component: Articles, meta: { keepAlive: true } },
+    { path: '/todo', name: 'TodoIndex', component: TodoIndex, meta: { keepAlive: true } },
+    { path: '/plan', name: 'PlanIndex', component: PlanIndex, meta: { keepAlive: false } },
+    { path: '/note', name: 'NoteIndex', component: NoteIndex, meta: { keepAlive: false } }
   ]
 })
