d28d811a868d4616be1cfc59279e7992193dfeb4
xiaozzzi <42293085+xiaozzzi@users.noreply.github.com>
2023-11-20 16:46:43 +0800

    修改待办事项功能

--- blossom-web/src/views/todo/TodoIndex.vue
+++ blossom-web/src/views/todo/TodoIndex.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="todo-root">
-    <div class="header">
-      <IndexHeader :bg="true"></IndexHeader>
-    </div>
-
     <bl-row just="space-between" class="workbench" height="45px">
       <el-radio-group v-model="todoType" size="small">
         <el-radio-button :label="10">每日</el-radio-button>
@@ -21,11 +17,11 @@
     <el-calendar v-show="todoType === 10" v-model="calendarDate" class="task-day-calendar" ref="CalendarRef">
       <template #header="{ date }"><div></div></template>
       <template #date-cell="{ data }">
-        <bl-col just="space-around" class="cell-wrapper" @click="toTask(data.day)">
+        <bl-col just="flex-start" class="cell-wrapper" @click="toTask(data.day)">
           <div class="day">{{ data.day.split('-')[2] }}</div>
-          <div v-if="getCount(data.day) > 0">
+          <bl-row just="flex-end" class="count" v-if="getCount(data.day) > 0">
             <bl-tag :size="14">{{ getCount(data.day) }}</bl-tag>
-          </div>
+          </bl-row>
         </bl-col>
       </template>
     </el-calendar>
@@ -58,77 +54,142 @@
 
     <!--  -->
     <bl-col class="tasks" just="flex-start" align="center">
-      <div v-for="task in taskShow" class="task" @click="showDetail(task)">
-        <bl-row just="space-between">
+      <div v-for="task in taskShow" class="task" @click="showDetail('upd', task)">
+        <bl-row
+          class="process"
+          height="3px"
+          :width="Math.max(task.process, 0) + '%'"
+          :style="{ backgroundColor: `${isBlank(task.color) ? '#E5E5E5' : task.color}`, borderTopRightRadius: task.process >= 100 ? '4px' : '' }">
+        </bl-row>
+        <bl-row style="padding: 0 10px" just="space-between">
           <div class="title">{{ task.taskName }}</div>
           <div class="title">{{ task.deadLine }}</div>
         </bl-row>
         <div class="content">{{ task.taskContent }}</div>
-        <div class="cornor" :style="{ borderTop: `20px solid ${isBlank(task.color) ? '#E5E5E5' : task.color}` }"></div>
       </div>
     </bl-col>
+    <!--  -->
+    <div class="task-add" @click="showDetail('add')"><span class="iconbl bl-a-addline-line"></span></div>
   </div>
 
   <!--  -->
-  <el-drawer v-model="isShowDetail" direction="btt" :with-header="true" size="345px">
-    <template #header="{ close, titleId, titleClass }">
-      <div class="drawer-header">
-        <el-input style="--el-input-border-color: #ffffff00" v-model="curTask.taskName"></el-input>
-      </div>
-    </template>
+  <el-drawer v-model="isShowDetail" direction="btt" :with-header="true" size="400px">
     <div class="detail">
-      <el-input type="textarea" placeholder="无内容" v-model="curTask.taskContent" resize="none" :rows="4"></el-input>
-      <bl-row class="tags">
-        <bl-tag v-for="tag in curTask.taskTags" :bg-color="isBlank(curTask.color) ? '#000' : curTask.color" :size="13" style="min-height: 20px">
-          {{ tag }}
-        </bl-tag>
+      <el-input class="task-name" size="small" style="width: calc(100% - 30px)" v-model="curTask.taskName" placeholder="Todo 标题">
+        <template #prefix>
+          <el-icon size="15">
+            <Document />
+          </el-icon>
+        </template>
+      </el-input>
+      <el-input size="small" type="textarea" placeholder="Todo 内容" v-model="curTask.taskContent" resize="none" :rows="4"></el-input>
+      <!--  -->
+      <bl-row class="times">
+        <div class="label">截止：</div>
+        <el-input size="small" style="width: calc(100% - 50px)" v-model="curTask.deadLine" placeholder="..."></el-input>
       </bl-row>
-      <bl-col class="times">
-        <bl-row style="color: red">截止至：{{ curTask.deadLine }}</bl-row>
-        <!-- <bl-row>创建于：{{ curTask.creTime }} </bl-row>
-        <bl-row>开始于：{{ curTask.startTime }} </bl-row>
-        <bl-row>完成于：{{ curTask.endTime }} </bl-row> -->
-      </bl-col>
+      <!--  -->
+      <bl-row class="colors">
+        <div class="label" :style="{ color: curTask.color }">颜色：</div>
+        <div
+          v-for="color in colors"
+          class="color"
+          :key="color"
+          :style="{ backgroundColor: color }"
+          @click="
+            () => {
+              curTask.color = color
+            }
+          "></div>
+      </bl-row>
+      <!--  -->
+      <bl-row class="tags">
+        <el-button @click="showTagDialog" size="small"> + 标签 </el-button>
+        <el-tag
+          v-for="tag in curTask.taskTags"
+          :key="tag"
+          :disable-transitions="false"
+          :color="isBlank(curTask.color) ? '#474747' : curTask.color"
+          @close="handleTagClose(tag)"
+          closable>
+          {{ tag }}
+        </el-tag>
+      </bl-row>
+      <!--  -->
       <bl-row>
-        <el-slider v-model="curTask.process" :show-tooltip="false" size="small" />
+        <el-slider
+          v-model="curTask.process"
+          :show-tooltip="false"
+          size="small"
+          :style="{ '--el-slider-main-bg-color': isBlank(curTask.color) ? '#474747' : curTask.color }" />
         <div class="process">{{ curTask.process }}%</div>
       </bl-row>
       <bl-row class="btns" just="space-between">
         <el-button-group>
-          <el-button @click="updTask"><i class="iconbl bl-a-texteditorsave-line"></i></el-button>
-          <el-button @click="updTask">123</el-button>
+          <el-button v-if="detailType === 'upd'" color="#474747" @click="delTask"><i class="iconbl bl-delete-line"></i></el-button>
+          <el-button v-if="detailType === 'upd'" color="#474747" @click="updTask"><i class="iconbl bl-a-texteditorsave-line"></i></el-button>
         </el-button-group>
+        <el-button v-if="detailType === 'add'" color="#474747" style="width: 100%" @click="addTask">保 存</el-button>
         <el-button-group>
-          <el-button color="#000" v-if="curTask.taskStatus === 'WAITING'" @click="toProcessing">
+          <el-button v-if="detailType === 'upd' && curTask.taskStatus !== 'WAITING'" color="#E2EBD0" @click="toWaiting">
+            <span class="iconbl bl-a-boxsubtract-line"></span>待 办
+          </el-button>
+          <el-button v-if="detailType === 'upd' && curTask.taskStatus !== 'PROCESSING'" color="#E2EBD0" @click="toProcessing">
             <span class="iconbl bl-a-boxaddition-line"></span>开 始
           </el-button>
-          <el-button color="#000" v-if="curTask.taskStatus === 'WAITING' || curTask.taskStatus === 'PROCESSING'" @click="toCompleted">
+          <el-button v-if="detailType === 'upd' && curTask.taskStatus !== 'COMPLETED'" color="#E2EBD0" @click="toCompleted">
             <span class="iconbl bl-a-boxchoice-line"></span>完 成
           </el-button>
         </el-button-group>
       </bl-row>
       <div class="status">
-        {{ curTask.taskStatus }}
+        {{ taskStatus }}
       </div>
     </div>
   </el-drawer>
+
+  <el-dialog v-model="isShowTagDialog" :append-to-body="true" :destroy-on-close="true" :close-on-click-modal="false" title="添加标签">
+    <el-input v-model="tagInput" placeholder="标签内容..."></el-input>
+    <el-button style="width: 100%; margin-top: 10px" color="#474747" @click="addTag">添加</el-button>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
-import { ElMessage } from 'element-plus'
+import { Document } from '@element-plus/icons-vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
 import type { CalendarDateType, CalendarInstance } from 'element-plus'
-import { tasksApi, todosApi, toProcessingApi, toCompletedApi, updTaskApi } from '@/api/todo'
+import { tasksApi, todosApi, toWaitingApi, toProcessingApi, toCompletedApi, addTaskApi, updTaskApi, delTaskApi } from '@/api/todo'
 import type { TodoList, TaskInfo, TodoType, TaskStatus } from './scripts/types'
 import { isBlank } from '@/assets/utils/obj'
-import IndexHeader from '@/views/index/IndexHeader.vue'
 
 onMounted(() => {
   getTodos()
 })
 
-//#region ----------------------------------------< 待办事项 >--------------------------------------
+const updatePillPosition = () => {
+  Array.from(document.querySelectorAll('.segmented-control .option input')).forEach((elem: Element, index) => {
+    let input = elem as HTMLInputElement
+    if (input.checked) {
+      let selection = document.querySelector('.segmented-control .selection')
+      if (selection) {
+        ;(selection as HTMLElement).style.transform = 'translateX(' + input.offsetWidth * index + 'px)'
+      }
+    }
+  })
+}
 
+const colors = ref([
+  'rgba(68, 173, 56, 0.7)',
+  'rgba(186, 196, 44, 0.7)',
+  'rgba(235, 205, 72, 0.7)',
+  'rgba(232, 144, 144, 0.7)',
+  'rgba(112, 145, 188, 0.7)',
+  'rgba(157, 129, 216, 0.7)',
+  'rgba(0, 0, 0, 0.65)'
+])
+
+//#region ----------------------------------------< 待办事项 >--------------------------------------
 const calendarDate = ref<Date>(new Date())
 const CalendarRef = ref<CalendarInstance>()
 const selectDate = (val: CalendarDateType) => {
@@ -167,25 +228,29 @@ const getCount = (day: string): number => {
 //#endregion
 
 //#region ----------------------------------------< 任务 >--------------------------------------
+const initTask = (): TaskInfo => {
+  return {
+    id: '',
+    todoId: '',
+    todoName: '',
+    todoType: 10,
+    taskName: '',
+    taskContent: '',
+    taskTags: [],
+    deadLine: '',
+    creTime: '',
+    startTime: '',
+    endTime: '',
+    process: 0,
+    color: '',
+    taskStatus: 'WAITING',
+    updTaskName: false,
+    updTaskContent: false
+  }
+}
 // 当前待办事项
-const curTask = ref<TaskInfo>({
-  id: '',
-  todoId: '',
-  todoName: '',
-  todoType: 10,
-  taskName: '',
-  taskContent: 'content 张三李四',
-  taskTags: ['张三', '李四', 'Article'],
-  deadLine: '今天下午两点30分',
-  creTime: '2023-01-01 12:21:32',
-  startTime: '2023-01-01 12:21:32',
-  endTime: '2023-01-01 12:21:32',
-  process: 0,
-  color: '#000',
-  taskStatus: 'WAITING',
-  updTaskName: false,
-  updTaskContent: false
-})
+const curTodoId = ref<string>('')
+const curTask = ref<TaskInfo>(initTask())
 const taskWait = ref<TaskInfo[]>([])
 const taskProc = ref<TaskInfo[]>([])
 const taskComp = ref<TaskInfo[]>([])
@@ -204,26 +269,23 @@ const taskShow = computed(() => {
     return taskComp.value.filter((t: { todoType: number }) => t.todoType != 99)
   }
 })
-
-const updatePillPosition = () => {
-  Array.from(document.querySelectorAll('.segmented-control .option input')).forEach((elem: Element, index) => {
-    let input = elem as HTMLInputElement
-    if (input.checked) {
-      let selection = document.querySelector('.segmented-control .selection')
-      if (selection) {
-        ;(selection as HTMLElement).style.transform = 'translateX(' + input.offsetWidth * index + 'px)'
-      }
-    }
-  })
-}
-
+const taskStatus = computed(() => {
+  if (curTask.value.taskStatus === 'WAITING') {
+    return '未开始'
+  }
+  if (curTask.value.taskStatus === 'PROCESSING') {
+    return '进行中'
+  }
+  if (curTask.value.taskStatus === 'COMPLETED') {
+    return '已完成'
+  }
+})
 /**
- * 查看指定待办事项
+ * 查看指定待办事项的所有任务
  * @param todoId 待办事项ID
- * @param todoName 待办事项名称
- * @param todoType 待办事项类型
  */
 const toTask = (todoId: string) => {
+  curTodoId.value = todoId
   tasksApi({ todoId: todoId }).then((resp) => {
     taskWait.value = resp.data.waiting
     taskProc.value = resp.data.processing
@@ -237,9 +299,17 @@ const savedCallback = (data: any) => {
   taskComp.value = data.completed
 }
 
+const toWaiting = () => {
+  toWaitingApi({ id: curTask.value.id!, todoId: curTask.value.todoId }).then((resp) => {
+    ElMessage('已移至待办')
+    savedCallback(resp.data)
+    isShowDetail.value = false
+  })
+}
+
 const toProcessing = () => {
   toProcessingApi({ id: curTask.value.id!, todoId: curTask.value.todoId }).then((resp) => {
-    ElMessage('任务已开始')
+    ElMessage('已移至进行中')
     savedCallback(resp.data)
     isShowDetail.value = false
   })
@@ -247,9 +317,26 @@ const toProcessing = () => {
 
 const toCompleted = () => {
   toCompletedApi({ id: curTask.value.id!, todoId: curTask.value.todoId }).then((resp) => {
-    ElMessage('任务已完成')
+    ElMessage('已移至完成')
     savedCallback(resp.data)
     isShowDetail.value = false
+  })
+}
+
+const addTask = () => {
+  if (isBlank(curTask.value.todoId)) {
+    ElMessage('请先选择要添加任务的日期或阶段性事项')
+    return
+  }
+  if (isBlank(curTask.value.taskName)) {
+    ElMessage('请填写 Todo 标题')
+    return
+  }
+  addTaskApi(curTask.value).then((resp) => {
+    ElMessage('新增成功')
+    isShowDetail.value = false
+    savedCallback(resp.data)
+    getTodos()
   })
 }
 
@@ -260,13 +347,60 @@ const updTask = () => {
   })
 }
 
-const isShowDetail = ref(false)
-const showDetail = (task: TaskInfo) => {
-  isShowDetail.value = true
-  curTask.value = task
+const delTask = () => {
+  ElMessageBox.confirm('是否确定删除该任务', '删除任务', {
+    confirmButtonText: '我要删除',
+    cancelButtonText: '取消'
+  }).then(() => {
+    delTaskApi({ id: curTask.value.id!, todoId: curTask.value.todoId }).then((resp) => {
+      ElMessage('删除成功')
+      savedCallback(resp.data)
+      isShowDetail.value = false
+    })
+  })
 }
 
-//#region 类型
+const detailType = ref('upd')
+const isShowDetail = ref(false)
+/**
+ * 显式任务详情
+ * @param type 新增/修改
+ * @param task 修改的任务
+ */
+const showDetail = (type: 'add' | 'upd', task?: TaskInfo) => {
+  isShowDetail.value = true
+  detailType.value = type
+  if (type === 'upd') {
+    curTask.value = task!
+  } else {
+    curTask.value = initTask()
+    curTask.value.todoType = todoType.value
+    curTask.value.todoId = curTodoId.value
+  }
+}
+
+//#endregion
+
+//#region ----------------------------------------< 标签 >--------------------------------------
+const isShowTagDialog = ref(false)
+const tagInput = ref('')
+const showTagDialog = () => {
+  isShowTagDialog.value = true
+}
+const handleTagClose = (tag: string) => {
+  curTask.value.taskTags.splice(curTask.value.taskTags.indexOf(tag), 1)
+}
+const addTag = () => {
+  if (isBlank(tagInput.value)) {
+    ElMessage('请填写标签内容')
+    return
+  }
+  curTask.value.taskTags.push(tagInput.value)
+  isShowTagDialog.value = false
+  tagInput.value = ''
+}
+
+//#endregion
 </script>
 
 <style scoped lang="scss">
@@ -274,19 +408,7 @@ const showDetail = (task: TaskInfo) => {
   @include box(100%, 100%);
   @include flex(column, flex-start, center);
   overflow: hidden;
-  --primary-light: #8abdff;
-  --primary: #6d5dfc;
-  --primary-dark: #5b0eeb;
-
-  --white: #ffffff;
-  --greyLight-1: #e4ebf5;
-  --greyLight-2: #c8d0e7;
-  --greyLight-3: #bec8e4;
-  --greyDark: #9baacf;
-
-  .header {
-    @include box(100%, 60px);
-  }
+  position: relative;
 
   .workbench,
   .task-day-calendar,
@@ -296,16 +418,18 @@ const showDetail = (task: TaskInfo) => {
     max-width: 900px;
   }
 
+  // 顶部操作台
   .workbench {
-    padding: 10px 10px;
+    padding: 0 10px 10px 10px;
 
     :deep(.el-button, .el-radio-button__inner) {
       padding: 8px 10px;
     }
   }
 
+  // 任务日历
   .task-day-calendar {
-    min-height: 230px;
+    min-height: 203px;
     background-color: transparent;
     --el-calendar-cell-width: 40px;
 
@@ -335,15 +459,21 @@ const showDetail = (task: TaskInfo) => {
         .current,
         .next {
           .el-calendar-day {
+            height: 35px;
             padding: 0;
             .cell-wrapper {
               @include box(100%, 100%);
+              position: relative;
               .day {
+                width: 100%;
                 @include font(14px, 300);
-                text-align: center;
+                padding: 1px 0 0 3px;
               }
-              .tag-root {
-                font-size: 14px;
+              .count {
+                text-align: right;
+                position: absolute;
+                right: 5px;
+                bottom: 3px;
               }
             }
           }
@@ -355,8 +485,9 @@ const showDetail = (task: TaskInfo) => {
     }
   }
 
+  // 阶段性事项列表
   .phaseds {
-    @include box(100%, 230px);
+    @include box(100%, 203px);
     @include flex(row, center, flex-start);
     align-content: flex-start;
     flex-wrap: wrap;
@@ -380,6 +511,7 @@ const showDetail = (task: TaskInfo) => {
     }
   }
 
+  // 三阶段控制按钮
   .segmented-control {
     --background: rgba(239, 239, 240, 1);
     background: var(--background);
@@ -497,6 +629,7 @@ const showDetail = (task: TaskInfo) => {
     }
   }
 
+  // 任务列表
   .tasks {
     width: 95%;
     max-width: 650px;
@@ -509,15 +642,20 @@ const showDetail = (task: TaskInfo) => {
     .task {
       width: 100%;
       font-size: 14px;
-      padding: 10px;
       margin-bottom: 10px;
       border-radius: 4px;
       background-color: #f3f3f3;
       position: relative;
       transition: background-color 0.3s;
+      padding-bottom: 10px;
 
       &:active {
         background-color: #d8d8d8;
+      }
+
+      .process {
+        margin-bottom: 7px;
+        border-top-left-radius: 4px;
       }
 
       .title {
@@ -533,6 +671,7 @@ const showDetail = (task: TaskInfo) => {
 
       .content {
         font-size: 0.8rem;
+        padding: 0 10px;
         margin-top: 3px;
         color: #a3a3a3;
         word-break: break-all;
@@ -541,46 +680,92 @@ const showDetail = (task: TaskInfo) => {
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
       }
+    }
+  }
 
-      .cornor {
-        // @include box(20px, 20px);
-        // transform: rotate(45deg);
-        // border-top-left-radius: 10px;
+  // 新增按钮
+  .task-add {
+    @include flex(row, center, center);
+    @include box(40px, 40px);
+    border-radius: 50%;
+    background-color: #7b7b7b4c;
+    box-shadow: 3px 3px 5px #bababa;
+    position: absolute;
+    right: 10px;
+    bottom: 5%;
+    cursor: pointer;
+    transition: background-color 0.3s;
 
-        position: absolute;
-        top: 0;
-        right: 0;
+    &:hover {
+      background-color: var(--el-color-primary-light-7);
+    }
 
-        border-left: 20px solid transparent;
-        border-top-right-radius: 4px;
-      }
+    .iconbl {
+      color: #7b7b7ba9;
+      font-size: 20px;
     }
   }
 }
 
 .drawer-header {
   --el-font-size-base: 15px;
+  position: relative;
 }
 
+// 抽屉的内容
 .detail {
-  @include flex(column, space-between, flex-start);
-  --el-font-size-base: 15px;
-  .el-textarea {
-    --el-input-border-color: #ffffff00 !important;
+  @include flex(column, flex-start, flex-start);
+  height: 100%;
+  padding-top: 20px;
+  padding-bottom: 30px;
+  --el-font-size-base: 14px;
+
+  & > div {
+    margin-bottom: 18px;
   }
 
   .tags {
     height: 44px;
-    padding: 10px 0 3px 0;
     overflow: scroll;
+    margin-bottom: 0;
+
+    .el-button {
+      width: 55px;
+      margin-right: 5px;
+    }
+    .el-tag {
+      --el-tag-text-color: #fff;
+      margin-right: 5px;
+    }
   }
 
   .times {
-    margin-top: 10px;
+    width: 100%;
     font-size: 13px;
+    color: var(--el-text-color-regular);
     .bl-row-root {
-      margin-bottom: 5px;
       white-space: nowrap;
+    }
+    .label {
+      width: 50px;
+    }
+  }
+
+  .colors {
+    height: 24px;
+    font-size: 13px;
+    color: var(--el-text-color-regular);
+    .color {
+      @include box(20px, 20px);
+      margin: 0 15px 0 0;
+      border-radius: 4px;
+      transition: transform 0.1s;
+      &:active {
+        transform: scale(1.2);
+      }
+    }
+    .label {
+      width: 50px;
     }
   }
 
@@ -592,18 +777,28 @@ const showDetail = (task: TaskInfo) => {
   }
 
   .btns {
-    margin-top: 10px;
+    margin-bottom: 0;
+    .bl-delete-line {
+      font-size: 20px;
+    }
+    .bl-a-texteditorsave-line {
+      font-size: 17px;
+    }
 
-    .iconbl {
+    .bl-a-boxsubtract-line,
+    .bl-a-boxaddition-line,
+    .bl-a-boxchoice-line {
       margin-right: 5px;
     }
   }
+
   .status {
     font-size: 12px;
     font-style: italic;
+    margin-bottom: 0;
     color: #bebebe;
     position: absolute;
-    right: 30px;
+    left: 30px;
     bottom: 10px;
   }
 }
